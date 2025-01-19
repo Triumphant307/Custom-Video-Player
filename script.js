7c72fe31bc997e5585d84d8380c05114bc8b1947
Triumphant307 <usuangbontriumphant@gmail.com>
2025-01-18 18:23:35 -0800

    slight changes

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+
+
 const playPauseBtn = document.querySelector(".play-pause-btn");
 const miniplayerbtn = document.querySelector(".mini-player-btn");
 const theaterbtn = document.querySelector(".theater-btn");
@@ -11,10 +13,11 @@ const currentTime = document.querySelector(".current-time");
 const totalTime = document.querySelector(".total-time");
 const volumeslider = document.querySelector(".volume-slider");
 const video = document.querySelector("video");
+const timeline = document.querySelector(".timeline");
 const timelineContainer = document.querySelector(".timeline-container");
 const videoContainer = document.querySelector(".videoContainer");
 
-video.textTracks[0].mode = "hidden"
+video.textTracks[0].mode = "hidden";
 
 document.addEventListener("keydown", (e) => {
   const tagName = document.activeElement.tagName.toLowerCase();
@@ -23,6 +26,7 @@ document.addEventListener("keydown", (e) => {
   switch (e.key.toLowerCase()) {
     case " ":
       if (tagName === "button") return;
+      e.preventDefault();
     case "k":
       tooglePlay();
       break;
@@ -33,7 +37,7 @@ document.addEventListener("keydown", (e) => {
       toogleTheaterMode();
       break;
     case "i":
-      tooogleMiniPlayerMode();
+      if (!(e.ctrlKey && e.shiftKey)) tooogleMiniPlayerMode();
       break;
     case "m":
       toogleMute();
@@ -49,75 +53,91 @@ document.addEventListener("keydown", (e) => {
 });
 
 //Timeline
-timelineContainer.addEventListener("mousemove" ,handleTimelineUpdate )
-timelineContainer.addEventListener("mousedown" , toogleScrubbing)
-document.addEventListener("mouseup" , e =>{
-  if (isSrcubbing) {
-    toogleScrubbing(e)
+timelineContainer.addEventListener("mousemove", handleTimelineUpdate);
+timelineContainer.addEventListener("mousedown", toggleScrubbing);
+document.addEventListener("mouseup", (e) => {
+  if (isScrubbing) {
+    toggleScrubbing(e);
   }
-})
+});
 document.addEventListener("mousemove", (e) => {
-  if (isSrcubbing) {
+  if (isScrubbing) {
     handleTimelineUpdate(e);
   }
 });
 
-let isSrcubbing = false
-let wasPaused
-function toogleScrubbing(e){
-      const rect = timelineContainer.getBoundingClientRect();
-      const percent =
-        Math.min(Math.max(0, e.x - rect.x), rect.width) / rect.width;
-        isSrcubbing = (e.buttons & 1) === 1
-        videoContainer.classList.toggle("scrubbing" , isSrcubbing)
-        if (isSrcubbing) {
-          wasPaused = video.pause()
-        }
-        else{
-          video.currentTime = percent * video.duration
-          if (!wasPaused) {
-            video.play()
-          }
-        }
-}
-function handleTimelineUpdate(e){
-      const rect = timelineContainer.getBoundingClientRect()
-      const percent = Math.min(Math.max(0 , e.x - rect.x) , rect.width)
-/rect.width
-const previewImgNumber = Math.max(1 , Math.floor((percent * video.duration) / 10))
-const previewImgSrc = `../assets/previewimages/previewimages${previewImgNumber}.jpg`;
-previewImg.src = previewImgSrc
-  timelineContainer.style.setProperty("--preview-position", percent);
+let isScrubbing = false;
+let wasPaused;
+timelineContainer.addEventListener("pointerdown", e => {
+  timelineContainer.setPointerCapture(e.pointerId);
+  isScrubbing = true;
+  toggleScrubbing(e)
+  timelineContainer.addEventListener("pointermove", handleTimelineUpdate);
+  timelineContainer.addEventListener("pointerup", e => {
+    isScrubbing = false;
+    toggleScrubbing(e)
+    timelineContainer.removeEventListener("pointermove", handleTimelineUpdate);
+  }, {once: true});
+});
 
-  if(isSrcubbing){
-    e.preventDefault()
-    thumbnailImg.src = previewImgSrc
-    timelineContainer.style.setProperty("--progress-position" , percent)
+function toggleScrubbing(e) {
+  const rect = timelineContainer.getBoundingClientRect();
+  const percent = Math.min(Math.max(0, e.x - rect.x), rect.width) / rect.width;
+  videoContainer.classList.toggle("scrubbing", isScrubbing);
+  if (isScrubbing) {
+    wasPaused = video.pause();
+  } else {
+    video.currentTime = percent * video.duration;
+    if (!wasPaused) {
+      video.play();
+    }
+  }
+}
+function clamp(min, value, max) {
+  return Math.max(min, Math.min(value, max))
+}
+function handleTimelineUpdate(e) {
+  const rect = timelineContainer.getBoundingClientRect();
+  let percent = Math.min(Math.max(0, e.x - rect.x), rect.width) / rect.width;
+  const previewImgNumber = Math.max(
+    1,
+    Math.floor((percent * video.duration) / 10)
+  );
+  const min = (previewImg.offsetWidth / 2) / rect.width
+  const max = 1 - min
+  const previewPercent = clamp(min, percent, max);
+  const previewImgSrc = `../assets/previewimages/previewimages${previewImgNumber}.jpg`;
+  previewImg.src = previewImgSrc;
+  timelineContainer.style.setProperty("--preview-position", percent);
+  timelineContainer.style.setProperty("--preview-img-position", previewPercent);
+
+  if (isScrubbing) {
+    e.preventDefault();
+    thumbnailImg.src = previewImgSrc;
+    timelineContainer.style.setProperty("--progress-position", percent);
   }
 }
 //playback speed
-speedBtn.addEventListener("click" , changePlaybackSpeed)
+speedBtn.addEventListener("click", changePlaybackSpeed);
 
-function changePlaybackSpeed(){
-  let newPlayback = video.playbackRate + 0.25
-  if(newPlayback > 2) newPlayback = 0.25
-  video.playbackRate = newPlayback
-  speedBtn.textContent = `${newPlayback}x`
-
+function changePlaybackSpeed() {
+  let newPlayback = video.playbackRate + 0.25;
+  if (newPlayback > 2) newPlayback = 0.25;
+  video.playbackRate = newPlayback;
+  speedBtn.textContent = `${newPlayback}x`;
 }
 
 //caption
-  const caption = video.textTracks[0]
-  caption.mode = "hidden";
+const caption = video.textTracks[0];
+caption.mode = "hidden";
 
-  captionBtn.addEventListener("click" , toogleCaption)  
+captionBtn.addEventListener("click", toogleCaption);
 
-  function toogleCaption(){
-    const isHidden = caption.mode === "hidden"
-    caption.mode = isHidden ? "showing" : "hidden"
-    videoContainer.classList.toggle("captions" , isHidden)
-  }
-
+function toogleCaption() {
+  const isHidden = caption.mode === "hidden";
+  caption.mode = isHidden ? "showing" : "hidden";
+  videoContainer.classList.toggle("captions", isHidden);
+}
 
 //Duration
 video.addEventListener("loadeddata", () => {
@@ -126,9 +146,9 @@ video.addEventListener("loadeddata", () => {
 
 video.addEventListener("timeupdate", () => {
   currentTime.textContent = formatDuration(video.currentTime);
-  const percent = video.currentTime / video.duration
+  const percent = video.currentTime / video.duration;
 
-      timelineContainer.style.setProperty("--progress-position", percent);
+  timelineContainer.style.setProperty("--progress-position", percent);
 });
 
 const leadingZeroFormatter = new Intl.NumberFormat(undefined, {
@@ -182,6 +202,8 @@ video.addEventListener("volumechange", () => {
 });
 
 //view modes
+// document.addEventListener("scroll" , tooogleMiniPlayerMode)
+
 theaterbtn.addEventListener("click", toogleTheaterMode);
 fullScreen.addEventListener("click", tooogleFullScreenMode);
 miniplayerbtn.addEventListener("click", tooogleMiniPlayerMode);
